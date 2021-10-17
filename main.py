44e7fd6909880f7fadd3a0f54640163b2936b70a
Vamsi-cse-c-141 <92581688+Vamsi-cse-c-141@users.noreply.github.com>
2021-10-17 19:50:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('Salary_Data.csv')
+dataset = pd.read_csv('Real estate.csv')
 
-X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 1].values
+X = dataset.iloc[:, 3].values
+y = dataset.iloc[:, -1].values
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
@@ -26,15 +26,15 @@ y_pred = regressor.predict(X_test)
 # Visualising the Training set results
 plt.scatter(X_train, y_train, color = 'red')
 plt.plot(X_train, regressor.predict(X_train), color = 'blue')
-plt.title('Salary vs Experience (Training set)')
-plt.xlabel('Years of Experience')
-plt.ylabel('Salary')
+plt.title('House Age vs Price (Training set)')
+plt.xlabel('Age of House')
+plt.ylabel('Price of the House')
 plt.show()
 
 # Visualising the Test set results
 plt.scatter(X_test, y_test, color = 'red')
 plt.plot(X_train, regressor.predict(X_train), color = 'blue')
-plt.title('Salary vs Experience (Test set)')
-plt.xlabel('Years of Experience')
-plt.ylabel('Salary')
+plt.title('House Age vs Price (Test set)')
+plt.xlabel('Age of House')
+plt.ylabel('Price of the House')
 plt.show()
